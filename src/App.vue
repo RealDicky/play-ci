c15fb863a18b876cfa4cbdc94f11b60ccc88aef6
dingding <1337176014@qq.com>
2022-05-28 11:32:01 +0800

    :wq  a s da s

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;https://github.com/RealDicky/play-ci/blob/main/src/App.vue
+  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
